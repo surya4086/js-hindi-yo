7c4eae38cd987868d11bbe8bec1c48257a3f39fe
suraj kumar <165416963+surya4086@users.noreply.github.com>
2026-01-22 18:09:12 +0000

    yes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//we are using nodejs, not browser
+console.log(3
++3)//code readability should be high
+
+let name="suraj"
+let age=18
+let isloggein = false
+//number=>2 to power53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=> standalone value
+//undefined=>apne aap me a variable type hai
+//symbol=> unique
+
+
+//object
+
+console.log(typeof undefined);//undefine
+console.log(typeof null);
