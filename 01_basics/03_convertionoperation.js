c1165339ab15f008b297e55c4e86eed417e986dd
suraj kumar <165416963+surya4086@users.noreply.github.com>
2026-01-22 18:44:23 +0000

    yes

---
+++ 01_basics/03_convertionoperation.js
@@ -0,0 +1,28 @@
+let score="suraj"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueinnumber=number(score)
+//console.log(typeof valueinnumber);
+//console.log(valueinnumber);
+
+//"33"=>33
+//"33abc"=>nan
+//true=>1; false=>0
+
+let isloggedin="hitesh"
+
+let booleanisloggedin = boolean(isloggedin)
+
+//console.log(booleanisloggedin);
+//1=>true; 0=>false
+//""=>false
+//"hitesh"=>true
+
+let somenumber=33
+
+let stringnumber=string(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
